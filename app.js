20ca4977b8d9b961fb49802647e9dd6987e85528
joe-ashwell <67185069+joe-ashwell@users.noreply.github.com>
2021-01-05 18:50:37 +0000

    Add basic drag & drop functionality

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ class PuzzlePiece {
 }
 
 class PuzzlePiecePanel {
-  
   puzzleImageList = [
     // Row 1
     new PuzzlePiece('gorilla_img/gorilla_row1_col1.jpg'),
@@ -31,27 +30,77 @@ class PuzzlePiecePanel {
 
   constructor() {}
 
+  randomArraySort() {
+    const sortedPuzzleArray = this.puzzleImageList.sort( () => {
+    return 0.5 - Math.random();
+    });  
+    return sortedPuzzleArray;
+  }
+
   render() {
     const puzzlePiecePanel = document.querySelector('section.selection-panel');
-    for (const puzzlePiece in this.puzzleImageList) {
+
+    this.randomArraySort().forEach(puzzlePiece => {
       const puzzlePieceItem = document.createElement('div');
-      puzzlePieceItem.style.backgroundImage = `url('${this.puzzleImageList[puzzlePiece].url}')`;
+      puzzlePieceItem.style.backgroundImage = `url('${puzzlePiece.url}')`;
       puzzlePieceItem.draggable = true;
       puzzlePieceItem.classList.add('puzzle-piece');
+      puzzlePieceItem.addEventListener('dragstart', App.dragStart.bind(this));
       puzzlePiecePanel.append(puzzlePieceItem);
-      console.log(`url('${this.puzzleImageList[puzzlePiece].url}')`);
-    }
+    });
+
     return puzzlePiecePanel;
   }
 }
 
-class App {
+class PuzzleMain {
+  constructor() {}
 
+  render() {
+    const mainPuzzleSection = document.querySelector('section.puzzle-panel');
+
+    for (let i = 0; i < new PuzzlePiecePanel().puzzleImageList.length; i++) {
+      const puzzlePieceLocation = document.createElement('div');
+      puzzlePieceLocation.classList.add('puzzle-piece-location');
+      puzzlePieceLocation.addEventListener('dragenter', App.dragEnter.bind(this));
+      puzzlePieceLocation.addEventListener('dragover', App.dragOver.bind(this));
+      puzzlePieceLocation.addEventListener('dragleave', App.dragLeave.bind(this));
+      puzzlePieceLocation.addEventListener('drop', App.drop.bind(this));
+      mainPuzzleSection.append(puzzlePieceLocation);
+    }
+    return mainPuzzleSection;
+  }
+}
+
+class App {
   static init() {
-    const panel = new PuzzlePiecePanel();
-    panel.render();
+    new PuzzlePiecePanel().render();
+    new PuzzleMain().render();
   }
 
+  static dragStart(e) {
+    e.dataTransfer.setData('text/plain', e.target.style.backgroundImage);
+    // console.log(e.target.style.backgroundImage);
+  }
+
+  static dragEnter(e) {
+    e.preventDefault();
+    // e.target.style.backgroundColor = 'yellow';
+  }
+
+  static dragOver(e) {
+    e.preventDefault();
+    // e.target.style.backgroundColor = `red`;
+  }
+
+  static dragLeave(e) {
+    // e.target.style.backgroundColor = `blue`;
+  }
+
+  static drop(e) {
+    const URL = e.dataTransfer.getData('text/plain');
+    e.target.style.backgroundImage = `${URL}`;
+  }
 }
 
 App.init();
